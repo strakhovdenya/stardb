d66eba380aa86a9d04f7bac9cbeca444fa13e6d2
Denis <strakhov.denya@gmail.com>
2019-08-18 17:59:48 +0300

    add styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -79,7 +79,7 @@ export default class App extends Component {
                                            />
                                        )}
                                 />
-                                <Route render={() => (<h2>Page not found</h2>)}/>
+                                <Route render={() => (<div className="jumbotron"><h2>Page not found</h2></div>)}/>
                             </Switch>
                         </div>
                     </Router>
